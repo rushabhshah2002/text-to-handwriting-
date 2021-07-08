4ffc3cf18e4da5b6e3882ae665aa21a833c7b59a
rushabhshah2002 <rushabh.s1@ahduni.edu.in>
2021-07-08 22:23:16 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Text_handwritnig from "./text_handwriting"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <Text_handwritnig/>
+
     </div>
   );
 }
-
 export default App;
+
